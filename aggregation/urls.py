db95cbc8e1e1854a345e1d6216f3ce83bab640fa
unknown <ffarzam_1992@yahoo.com>
2023-12-11 15:29:26 +0330

    chore: add aggregation app related urls

---
+++ aggregation/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('site/', views.FillSiteTableView.as_view(), name="site"),
+    path('city/', views.FillCityTableView.as_view(), name="city"),
+    path('province/', views.FillCityTableView.as_view(), name="province"),
+]

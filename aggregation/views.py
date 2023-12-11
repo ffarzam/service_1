5dd6c686360445066a78cf9aaa8cba754f291575
unknown <ffarzam_1992@yahoo.com>
2023-12-11 15:27:57 +0330

    refactor: make views cleaner

--- aggregation/views.py
+++ aggregation/views.py
@@ -2,11 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from dumpdata.models import Raw
 
 from .models import Site, City
 
-from config import settings
+from .utils import get_related_queryset
 
 
 # Create your views here.
@@ -15,27 +14,7 @@ from config import settings
 class FillSiteTableView(APIView):
 
     def get(self, request):
-        data = Raw.objects.values("site").annotate(kpi_1=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_1"]]("kpi_1"),
-                                                   kpi_2=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_2"]]("kpi_2"),
-                                                   kpi_3=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_3"]]("kpi_3"),
-                                                   kpi_4=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_4"]]("kpi_4"),
-                                                   kpi_5=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_5"]]("kpi_5"),
-                                                   kpi_6=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_6"]]("kpi_6"),
-                                                   kpi_7=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_7"]]("kpi_7"),
-                                                   kpi_8=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_8"]]("kpi_8"),
-                                                   kpi_9=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_9"]]("kpi_9"),
-                                                   kpi_10=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_10"]]("kpi_10"),
-                                                   kpi_11=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_11"]]("kpi_11"),
-                                                   kpi_12=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_12"]]("kpi_12"),
-                                                   kpi_13=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_13"]]("kpi_13"),
-                                                   kpi_14=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_14"]]("kpi_14"),
-                                                   kpi_15=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_15"]]("kpi_15"),
-                                                   kpi_16=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_16"]]("kpi_16"),
-                                                   kpi_17=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_17"]]("kpi_17"),
-                                                   kpi_18=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_18"]]("kpi_18"),
-                                                   kpi_19=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_19"]]("kpi_19"),
-                                                   kpi_20=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_20"]]("kpi_20"),
-                                                   )
+        data = get_related_queryset("site")
 
         site_obj =(Site(**item) for item in data)
 
@@ -46,30 +25,9 @@ class FillSiteTableView(APIView):
 class FillCityTableView(APIView):
 
     def get(self, request):
-        data = Raw.objects.values("city").annotate(kpi_1=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_1"]]("kpi_1"),
-                                                   kpi_2=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_2"]]("kpi_2"),
-                                                   kpi_3=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_3"]]("kpi_3"),
-                                                   kpi_4=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_4"]]("kpi_4"),
-                                                   kpi_5=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_5"]]("kpi_5"),
-                                                   kpi_6=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_6"]]("kpi_6"),
-                                                   kpi_7=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_7"]]("kpi_7"),
-                                                   kpi_8=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_8"]]("kpi_8"),
-                                                   kpi_9=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_9"]]("kpi_9"),
-                                                   kpi_10=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_10"]]("kpi_10"),
-                                                   kpi_11=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_11"]]("kpi_11"),
-                                                   kpi_12=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_12"]]("kpi_12"),
-                                                   kpi_13=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_13"]]("kpi_13"),
-                                                   kpi_14=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_14"]]("kpi_14"),
-                                                   kpi_15=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_15"]]("kpi_15"),
-                                                   kpi_16=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_16"]]("kpi_16"),
-                                                   kpi_17=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_17"]]("kpi_17"),
-                                                   kpi_18=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_18"]]("kpi_18"),
-                                                   kpi_19=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_19"]]("kpi_19"),
-                                                   kpi_20=settings.AGGREGATION_MAPPER[settings.AGGREGATION["kpi_20"]]("kpi_20"),
-                                                   )
+        data = get_related_queryset("city")
 
         city_obj =(City(**item) for item in data)
 
         City.objects.bulk_create(city_obj, ignore_conflicts=True)
         return Response({"message": "city table was created"}, status=status.HTTP_200_OK)
-

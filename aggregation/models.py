9155cf57cf382f0064647d295dc9cb72c51ee251
unknown <ffarzam_1992@yahoo.com>
2023-12-12 12:20:32 +0330

    chore: add get queryset with in operator function

--- aggregation/models.py
+++ aggregation/models.py
@@ -2,26 +2,48 @@ from django.db import models
 
 from dumpdata.models import KPI
 
+from .mixins import ItemsMixin
+
 
 # Create your models here.
 
 
-class Site(KPI):
+class Site(ItemsMixin, KPI):
     site = models.CharField(max_length=25, unique=True)
+
+    # def get_kpi(self, kpi):
+    #     field_object = self.__class__._meta.get_field(kpi)
+    #     return field_object.value_from_object(self)
+
+    @classmethod
+    def get_queryset_with_in_operator(cls, elements):
+        return cls.objects.filter(site__in=elements)
 
     def __str__(self):
         return f"{self.site}"
 
 
-class Province(KPI):
+class Province(ItemsMixin, KPI):
     province = models.CharField(max_length=100, unique=True)
+
+    # def get_kpi(self, kpi):
+    #     field_object = self.__class__._meta.get_field(kpi)
+    #     return field_object.value_from_object(self)
+
+    @classmethod
+    def get_queryset_with_in_operator(cls, elements):
+        return cls.objects.filter(province__in=elements)
 
     def __str__(self):
         return f"{self.province}"
 
 
-class City(KPI):
+class City(ItemsMixin, KPI):
     city = models.CharField(max_length=100, unique=True)
+
+    @classmethod
+    def get_queryset_with_in_operator(cls, elements):
+        return cls.objects.filter(city__in=elements)
 
     def __str__(self):
         return f"{self.city}"
